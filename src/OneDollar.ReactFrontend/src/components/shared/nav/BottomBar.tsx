a71cbe709c7965e3b1e060a7d6297c5c74537352
mr-woodapple <39863485+mr-woodapple@users.noreply.github.com>
2026-01-05 22:42:47 +0100

    Updated bottom bar component

--- src/OneDollar.ReactFrontend/src/components/shared/nav/BottomBar.tsx
+++ src/OneDollar.ReactFrontend/src/components/shared/nav/BottomBar.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { House, Plus, UserCog } from "lucide-react";
+import { ChartSpline, House, Plus, UserCog } from "lucide-react";
 
 type BottomBarProps = {
   onAddClick?: () => void;
@@ -9,19 +8,24 @@ type BottomBarProps = {
 export default function BottomBar({ onAddClick, onShowHome}: BottomBarProps){
 
   return (
-    <div className="apple-safe-area fixed bottom-0 left-0 z-50 w-full bg-white border-t border-gray-200">
-      <div className="grid grid-cols-3 h-full justify-items-center content-center m-2.5">
-        <Button onClick={() => onShowHome(true)} variant="ghost">
-          <House />
-        </Button>
+    <div className="apple-safe-area fixed bottom-0 left-0 z-50 w-full">
+      {/* <div className="absolute inset-0 backdrop-blur-lg mask-[linear-gradient(to_bottom,transparent,black_75%)]  -z-10" /> */}
+      <div className="flex flex-row p-4 space-x-2.5 justify-center">
+        <div className="main flex flex-row bg-black rounded-full p-0.5">
+          <div className="cursor-pointer bg-white p-4 rounded-full" onClick={() => onShowHome(true)}>
+            <House />
+          </div>
+          <div className="cursor-pointer p-4">
+            <ChartSpline color="white" />
+          </div>
+          <div className="cursor-pointer p-4" onClick={() => onShowHome(false)}>
+            <UserCog color="white" />
+          </div>
+        </div>
 
-        <Button onClick={onAddClick}>
-          <Plus />
-        </Button>
-
-        <Button onClick={() => onShowHome(false)} variant="ghost">
-          <UserCog />
-        </Button>
+        <div className="fab bg-purple-700 hover:bg-neutral-700 rounded-full p-4.5 cursor-pointer" onClick={onAddClick}>
+          <Plus color="white" />
+        </div>
       </div>
     </div>
   )
